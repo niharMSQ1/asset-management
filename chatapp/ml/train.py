8d821327e259763313333cdca6b85a2e4d1e6af3
unknown <nihar.m@sq1.security>
2025-03-05 12:20:55 +0530

    chat backend

--- chatapp/ml/train.py
+++ chatapp/ml/train.py
@@ -7,7 +7,6 @@ from torch.utils.data import Dataset, DataLoader
 from .ntk_utils import bag_of_words, tokenize, stem
 from .model import NeuralNet
 
-# Load intents
 with open('chatapp/ml/intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -15,7 +14,6 @@ all_words = []
 tags = []
 xy = []
 
-# Process intents
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
@@ -24,7 +22,6 @@ for intent in intents['intents']:
         all_words.extend(w)
         xy.append((w, tag))
 
-# Stemming and sorting words
 ignore_words = ['?', '.', '!']
 all_words = sorted(set(stem(w) for w in all_words if w not in ignore_words))
 tags = sorted(set(tags))
@@ -40,7 +37,6 @@ for (pattern_sentence, tag) in xy:
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-# Hyperparameters
 num_epochs = 6000
 batch_size = 128
 learning_rate = 0.0005
@@ -48,7 +44,6 @@ input_size = len(X_train[0])
 hidden_size = 128
 output_size = len(tags)
 
-# Dataset
 class ChatDataset(Dataset):
     def __init__(self):
         self.n_samples = len(X_train)
@@ -64,7 +59,6 @@ class ChatDataset(Dataset):
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
-# Model, loss, and optimizer
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
@@ -87,7 +81,6 @@ for epoch in range(num_epochs):
     if (epoch+1) % 100 == 0:
         print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
-# Save model
 data = {
     "model_state": model.state_dict(),
     "input_size": input_size,
